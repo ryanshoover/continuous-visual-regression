9afa74921a7d9358a73d3d17cb7dbc0faf8c7dbe
Ryan Hoover <ryan.hoover@wpengine.com>
2018-02-21 13:40:19 -0700

    add references to get passing tests

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const backstopConfig = {
 	config: 'backstop.json',
 };
 
+gulp.task( 'reference', () => backstopjs( 'reference', backstopConfig ) );
+
 gulp.task( 'test', () => backstopjs( 'test', backstopConfig ) );
 
 gulp.task( 'default', ['test'] );
